227d2f489b25aaab2df74e40c826e0aed2f1af44
Abe Fetterman <abe@nomiku.com>
2017-02-05 17:18:30 -0800

    Update tests on contact

--- test/contact.unit.js
+++ test/contact.unit.js
@@ -26,6 +26,24 @@ describe('ContactDecorator', function() {
       }).pubkey).to.equal('028ac783dab2f134946ed5c6b85eadc48ce745721a01eb071d6faaadcdea7b32d5');
     });
 
+    it('should use the supplied private key', function() {
+      var Contact = CryptoContact;
+      expect(Contact({
+        address: '127.0.0.1',
+        port: 1337,
+        privateKey: '3d9828d83318d5b1c8a92b50967bd956155d22197ae9d79ff7e4f0c3209db617'
+      }).keypair.getPrivateKey()).to.equal('3d9828d83318d5b1c8a92b50967bd956155d22197ae9d79ff7e4f0c3209db617');
+    });
+
+    it('should not enumerate keypair', function() {
+      var Contact = CryptoContact;
+      expect(JSON.stringify(Contact({
+        address: '127.0.0.1',
+        port: 1337,
+        privateKey: '3d9828d83318d5b1c8a92b50967bd956155d22197ae9d79ff7e4f0c3209db617'
+      })).indexOf('keypair')).to.equal(-1);
+    });
+
     it('should use the correct nodeID', function() {
       var Contact = CryptoContact;
       expect(Contact({
